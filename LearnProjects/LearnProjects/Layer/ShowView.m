dd6b624ec653d6ebb88f877fcd8604630e033356
张泽 <zhangzeStrong@126.com>
2016-05-06 14:56:50 +0800

    叠加模式
    图形上下文中，添加多个元素使用叠加模式调整展示层级和颜色

--- LearnProjects/LearnProjects/Layer/ShowView.m
+++ LearnProjects/LearnProjects/Layer/ShowView.m
@@ -14,6 +14,296 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+    [self drawImageForTranform];
+}
+
+
+#pragma mark - 上下文的变换
+- (void) drawImageForTranform
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    //保存图形上下文的初始状态
+    CGContextSaveGState(context);
+    
+    //形变第一步：图形上下文向右平移40
+    CGContextTranslateCTM(context, 100, 0);
+    
+    //形变第二步：缩放0.8
+    CGContextScaleCTM(context, 0.8, 0.8);
+    
+    
+    //形变第三步：旋转
+    CGContextRotateCTM(context, M_PI_4/4);
+    
+    UIImage *image = [UIImage imageNamed:@"timg.jpeg"];
+    
+//    [image drawInRect:CGRectMake(0, 0, 200, 200)];
+//    CGContextDrawImage(context, CGRectMake(0, 0, 200, 200), image.CGImage);
+    
+    /*在Core Graphics中坐标系的y轴正方向是向上的，坐标原点在屏幕左下角，y轴方向刚好和UIKit中y轴方向相反。而使用UIKit进行绘图之所以没有问题是因为UIKit中进行了处理，事实上对于其他图形即使使用Core Graphics绘制也没有问题，因为UIKit统一了编程方式。但是使用Core Graphics中内置方法绘制图像是存在这种问题的，如何解决呢？
+     
+     其实图形上下文只要沿着x轴旋转180度，然后向上平移适当的高度即可（但是注意不要沿着z轴旋转，这样得不到想要的结果）。可是通过前面介绍的CGContextRotateCTM方法只能通过沿着z轴旋转，此时不妨使用另外一种方法，那就是在y轴方向缩放-1，同样可以达到想要的效果：
+     UIImage *image=[UIImage imageNamed:@"image2.jpg"];
+     CGSize size=[UIScreen mainScreen].bounds.size;
+     CGContextSaveGState(context);
+     CGFloat height=450,y=50;
+     //上下文形变
+     CGContextScaleCTM(context, 1.0, -1.0);//在y轴缩放-1相当于沿着x张旋转180
+     CGContextTranslateCTM(context, 0, -(size.height-(size.height-2*y-height)));//向上平移
+     //图像绘制
+     CGRect rect= CGRectMake(10, y, 300, height);
+     CGContextDrawImage(context, rect, image.CGImage);
+     
+     CGContextRestoreGState(context);
+     */
+    
+    CGContextRestoreGState(context);
+}
+
+
+#define TILE_SIZE  10
+#pragma mark - 无颜色填充模式
+//填充瓷砖的回调函数（必须满足CGPatternCallbacks签名）
+void drawTile(void *info,CGContextRef context){
+    CGContextFillRect(context, CGRectMake(0, 0, TILE_SIZE, TILE_SIZE));
+    CGContextFillRect(context, CGRectMake(TILE_SIZE, TILE_SIZE, TILE_SIZE, TILE_SIZE));
+}
+-(void)drawBackgroundWithPattern
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    //设备无关的颜色空间
+    CGColorSpaceRef rgbSpace= CGColorSpaceCreateDeviceRGB();
+    //模式填充颜色空间
+    CGColorSpaceRef colorSpace=CGColorSpaceCreatePattern(rgbSpace);
+    //将填充色颜色空间设置为模式填充的颜色空间
+    CGContextSetFillColorSpace(context, colorSpace);
+    
+    //填充模式回调函数结构体
+    CGPatternCallbacks callback={0,&drawTile,NULL};
+    /*填充模式
+     info://传递给callback的参数
+     bounds:瓷砖大小
+     matrix:形变
+     xStep:瓷砖横向间距
+     yStep:瓷砖纵向间距
+     tiling:贴砖的方法（瓷砖摆放的方式）
+     isClored:绘制的瓷砖是否已经指定了颜色（对于无颜色瓷砖此处指定位false）
+     callbacks:回调函数
+     */
+    CGPatternRef pattern=CGPatternCreate(NULL, CGRectMake(0, 0, 2*TILE_SIZE, 2*TILE_SIZE), CGAffineTransformIdentity,2*TILE_SIZE+ 5,2*TILE_SIZE+ 5, kCGPatternTilingNoDistortion, false, &callback);
+    
+    CGFloat components[]={254.0/255.0,52.0/255.0,90.0/255.0,1.0};
+    //注意最后一个参数对于无颜色填充模式指定为当前颜色空间颜色数据
+    CGContextSetFillPattern(context, pattern, components);
+    //    CGContextSetStrokePattern(context, pattern, components);
+    UIRectFill(CGRectMake(0, 0, 320, 568));
+    
+    CGColorSpaceRelease(rgbSpace);
+    CGColorSpaceRelease(colorSpace);
+    CGPatternRelease(pattern);
+}
+
+#pragma mark - draw 有颜色填充模式
+
+void drawColoredTile(void *info,CGContextRef context) //创建填充用到的瓦片
+{
+//    CGContextRef context = UIGraphicsGetCurrentContext();
+    //有颜色填充，这里设置填充色
+    CGContextSetRGBFillColor(context,  254.0/255.0, 52.0/255.0, 90.0/255.0, 1);
+    CGContextFillRect(context, CGRectMake(0, 0, TILE_SIZE, TILE_SIZE));
+    CGContextFillRect(context, CGRectMake(TILE_SIZE, TILE_SIZE, TILE_SIZE, TILE_SIZE));
+}
+
+-(void)drawBackgroundWithColoredPattern
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    //设备无关的颜色空间
+//    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    //创建模式填充颜色空间，注意对于有颜色的填充模式，这里传NULL
+    CGColorSpaceRef colorSpace = CGColorSpaceCreatePattern(NULL);
+    //将模式色填充颜色空间设置为模式填充的颜色空间
+    CGContextSetFillColorSpace(context, colorSpace);
+    //填充模式回调函数结构体
+    CGPatternCallbacks callBack = {0,&drawColoredTile,NULL};
+    /*填充模式
+     info://传递给callback的参数
+     bounds:瓷砖大小
+     matrix:形变
+     xStep:瓷砖横向间距
+     yStep:瓷砖纵向间距
+     tiling:贴砖的方法
+     isClored:绘制的瓷砖是否已经指定了颜色(对于有颜色瓷砖此处指定位true)
+     callbacks:回调函数
+     */
+    CGPatternRef pattern=CGPatternCreate(NULL, CGRectMake(0, 0, 2*TILE_SIZE, 2*TILE_SIZE), CGAffineTransformIdentity,2*TILE_SIZE+ 5,2*TILE_SIZE+ 5, kCGPatternTilingNoDistortion, true, &callBack);
+    
+    CGFloat alpha=1;
+    //注意最后一个参数对于有颜色瓷砖指定为透明度的参数地址，对于无颜色瓷砖则指定当前颜色空间对应的颜色数组
+    CGContextSetFillPattern(context, pattern, &alpha);
+    
+    UIRectFill(CGRectMake(0, 0, 320, 568));
+    
+    //    CGColorSpaceRelease(rgbSpace);
+    CGColorSpaceRelease(colorSpace);
+    CGPatternRelease(pattern);
+    
+    
+}
+
+
+#pragma mark - draw 的叠加模式 
+- (void) drawRectByQuertz2D
+{
+    //获取图形上下文
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    //设置颜色空间
+//    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    //设置颜色
+    CGFloat compents[] = {
+        248.0/255.0,86.0/255.0,86.0/255.0,1
+    };
+    CGFloat compents2[] = {
+        15.0/255.0,127.0/255.0,127.0/255.0,1
+    };
+    CGFloat compents3[] = {
+        100.0/255.0,127.0/255.0,127.0/255.0,1,
+    };
+    
+    CGRect rect= CGRectMake(0, 130.0, 320.0, 50.0);
+    
+    
+//    CGRect rect3=CGRectMake(40.0, 50.0, 10.0, 250.0);
+//    CGRect rect4=CGRectMake(60.0, 50.0, 10.0, 250.0);
+//    CGRect rect5=CGRectMake(80.0, 50.0, 10.0, 250.0);
+//    CGRect rect6=CGRectMake(100.0, 50.0, 10.0, 250.0);
+//    CGRect rect7=CGRectMake(120.0, 50.0, 10.0, 250.0);
+//    CGRect rect8=CGRectMake(140.0, 50.0, 10.0, 250.0);
+//    CGRect rect9=CGRectMake(160.0, 50.0, 10.0, 250.0);
+//    CGRect rect10=CGRectMake(180.0, 50.0, 10.0, 250.0);
+//    CGRect rect11=CGRectMake(200.0, 50.0, 10.0, 250.0);
+    
+    
+
+    CGContextAddRect(context, rect);
+    CGContextSetFillColor(context, compents);
+    CGContextDrawPath(context, kCGPathFillStroke);
+    
+    CGRect rect1= CGRectMake(0, 390.0, 320.0, 50.0);
+    CGContextAddRect(context, rect1);
+    CGContextSetFillColor(context, compents2);
+    CGContextDrawPath(context, kCGPathFillStroke);
+    
+    CGRect rect2=CGRectMake(20, 50.0, 10.0, 500.0);
+    CGContextAddRect(context, rect2);
+    CGContextSetFillColor(context, compents3);
+    CGContextSetBlendMode(context, kCGBlendModeLuminosity);
+    CGContextDrawPath(context, kCGPathFillStroke);
+    
+    
+//    CGContextAddRect(context, rect3);
+//    CGContextAddRect(context, rect4);
+//    CGContextAddRect(context, rect5);
+//    CGContextAddRect(context, rect6);
+//    CGContextAddRect(context, rect7);
+//    CGContextAddRect(context, rect8);
+//    CGContextAddRect(context, rect9);
+//    CGContextAddRect(context, rect10);
+//    CGContextAddRect(context, rect11);
+    
+   
+    
+//    CGContextSetBlendMode(context, kCGBlendModePlusDarker);
+    
+    
+    
+    
+    
+    
+    
+}
+
+#pragma mark - draw 的叠加模式 UIKit
+- (void) drawRectByUIKitWithContext2
+{
+
+    CGRect rect= CGRectMake(0, 130.0, 320.0, 50.0);
+    CGRect rect1= CGRectMake(0, 390.0, 320.0, 50.0);
+    CGRect rect2=CGRectMake(20, 50.0, 10.0, 250.0);
+    CGRect rect3=CGRectMake(40.0, 50.0, 10.0, 250.0);
+    CGRect rect4=CGRectMake(60.0, 50.0, 10.0, 250.0);
+    CGRect rect5=CGRectMake(80.0, 50.0, 10.0, 250.0);
+    CGRect rect6=CGRectMake(100.0, 50.0, 10.0, 250.0);
+    CGRect rect7=CGRectMake(120.0, 50.0, 10.0, 250.0);
+    CGRect rect8=CGRectMake(140.0, 50.0, 10.0, 250.0);
+    CGRect rect9=CGRectMake(160.0, 50.0, 10.0, 250.0);
+    CGRect rect10=CGRectMake(180.0, 50.0, 10.0, 250.0);
+    CGRect rect11=CGRectMake(200.0, 50.0, 10.0, 250.0);
+    CGRect rect12=CGRectMake(220.0, 50.0, 10.0, 250.0);
+    CGRect rect13=CGRectMake(240.0, 50.0, 10.0, 250.0);
+    CGRect rect14=CGRectMake(260.0, 50.0, 10.0, 250.0);
+    CGRect rect15=CGRectMake(280.0, 50.0, 10.0, 250.0);
+    CGRect rect16=CGRectMake(30.0, 310.0, 10.0, 250.0);
+    CGRect rect17=CGRectMake(50.0, 310.0, 10.0, 250.0);
+    CGRect rect18=CGRectMake(70.0, 310.0, 10.0, 250.0);
+    CGRect rect19=CGRectMake(90.0, 310.0, 10.0, 250.0);
+    CGRect rect20=CGRectMake(110.0, 310.0, 10.0, 250.0);
+    CGRect rect21=CGRectMake(130.0, 310.0, 10.0, 250.0);
+    CGRect rect22=CGRectMake(150.0, 310.0, 10.0, 250.0);
+    CGRect rect23=CGRectMake(170.0, 310.0, 10.0, 250.0);
+    CGRect rect24=CGRectMake(190.0, 310.0, 10.0, 250.0);
+    CGRect rect25=CGRectMake(210.0, 310.0, 10.0, 250.0);
+    CGRect rect26=CGRectMake(230.0, 310.0, 10.0, 250.0);
+    CGRect rect27=CGRectMake(250.0, 310.0, 10.0, 250.0);
+    CGRect rect28=CGRectMake(270.0, 310.0, 10.0, 250.0);
+    CGRect rect29=CGRectMake(290.0, 310.0, 10.0, 250.0);
+    
+    [[UIColor yellowColor] set];
+    UIRectFill(rect);
+    
+    [[UIColor greenColor] setFill];
+    UIRectFill(rect1);
+    
+    [[UIColor redColor] setFill];
+    
+    UIRectFillUsingBlendMode(rect2, kCGBlendModeClear);
+    UIRectFillUsingBlendMode(rect3, kCGBlendModeColor);
+    UIRectFillUsingBlendMode(rect4, kCGBlendModeColorBurn);
+    UIRectFillUsingBlendMode(rect5, kCGBlendModeColorDodge);
+    UIRectFillUsingBlendMode(rect6, kCGBlendModeCopy);
+    UIRectFillUsingBlendMode(rect7, kCGBlendModeDarken);
+    UIRectFillUsingBlendMode(rect8, kCGBlendModeDestinationAtop);
+    UIRectFillUsingBlendMode(rect9, kCGBlendModeDestinationIn);
+    UIRectFillUsingBlendMode(rect10, kCGBlendModeDestinationOut);
+    UIRectFillUsingBlendMode(rect11, kCGBlendModeDestinationOver);
+    UIRectFillUsingBlendMode(rect12, kCGBlendModeDifference);
+    UIRectFillUsingBlendMode(rect13, kCGBlendModeExclusion);
+    UIRectFillUsingBlendMode(rect14, kCGBlendModeHardLight);
+    UIRectFillUsingBlendMode(rect15, kCGBlendModeHue);
+    UIRectFillUsingBlendMode(rect16, kCGBlendModeLighten);
+    
+    UIRectFillUsingBlendMode(rect17, kCGBlendModeLuminosity);
+    UIRectFillUsingBlendMode(rect18, kCGBlendModeMultiply);
+    UIRectFillUsingBlendMode(rect19, kCGBlendModeNormal);
+    UIRectFillUsingBlendMode(rect20, kCGBlendModeOverlay);
+    UIRectFillUsingBlendMode(rect21, kCGBlendModePlusDarker);
+    UIRectFillUsingBlendMode(rect22, kCGBlendModePlusLighter);
+    UIRectFillUsingBlendMode(rect23, kCGBlendModeSaturation);
+    UIRectFillUsingBlendMode(rect24, kCGBlendModeScreen);
+    UIRectFillUsingBlendMode(rect25, kCGBlendModeSoftLight);
+    UIRectFillUsingBlendMode(rect26, kCGBlendModeSourceAtop);
+    UIRectFillUsingBlendMode(rect27, kCGBlendModeSourceIn);
+    UIRectFillUsingBlendMode(rect28, kCGBlendModeSourceOut);
+    UIRectFillUsingBlendMode(rect29, kCGBlendModeXOR);
+    
+}
+
+
 #pragma mark - draw line method 1
 - (void)drawLine1
 {
@@ -333,12 +623,6 @@
     CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, compents, locations, 3);
     //进行绘制渐变色
     CGContextDrawLinearGradient(context, gradient, CGPointMake(20, 50), CGPointMake(300, 300), kCGGradientDrawsBeforeStartLocation);
-}
-
-
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-    [self drawRectFillWithGradient];
 }
 
 
