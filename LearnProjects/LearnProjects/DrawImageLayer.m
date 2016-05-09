88f9b18514318955f7f3b21b6cdb57312f52b1ca
张泽 <zhangzeStrong@126.com>
2016-05-09 18:11:03 +0800

    CALayer绘图
    CALayer绘图

--- LearnProjects/LearnProjects/DrawImageLayer.m
+++ LearnProjects/LearnProjects/DrawImageLayer.m
@@ -7,7 +7,7 @@
 //
 
 #import "DrawImageLayer.h"
-
+#define PHOTO_HEIGHT 150
 @interface DrawImageLayer ()
 
 @end
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
-    
+    [self createLayer];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -30,31 +30,55 @@
 {
     CALayer *layer = [[CALayer alloc] init];
     //设置层大小
-    layer.bounds = CGRectMake(0, 0, 200, 200);
+    layer.bounds = CGRectMake(0, 0, 150, 150);
     
     //设置层的中心点
-    layer.position = CGPointMake(300, 300);
+    layer.position = CGPointMake(200, 200);
     
     //设置层的圆角
-    layer.cornerRadius = 200/2;
+    layer.cornerRadius = 150/2;
     
     layer.masksToBounds = YES;
     
+    //设置边框宽度
     layer.borderWidth = 2;
+    //设置边框颜色
     layer.borderColor = [UIColor whiteColor].CGColor;
+    //设置阴影颜色
     layer.shadowColor = [UIColor grayColor].CGColor;
-    layer.shadowOffset = CGSizeMake(10, 10);
+    //设置阴影区域
+    layer.shadowOffset = CGSizeMake(10, 50);
+    //设置阴影透明度
     layer.shadowOpacity = 1;
     
     [self.view.layer addSublayer:layer];
+    
+    //设置图层的代理
+    layer.delegate = self;
+    
     [layer setNeedsDisplay];
 }
 
-#pragma mark - layer delegate
+#pragma mark - layer delegate 绘制图形、图像到图层，注意参数中的ctx是图层的图形上下文，其中绘图位置也是相对图层而言的
 - (void)drawLayer:(CALayer *)layer
         inContext:(CGContextRef)ctx
 {
-//    CGContextRef 
+    
+    //    NSLog(@"%@",layer);//这个图层正是上面定义的图层
+    CGContextSaveGState(ctx);
+    
+    //图形上下文形变，解决图片倒立的问题
+    CGContextScaleCTM(ctx, 1, -1);
+    CGContextTranslateCTM(ctx, 0, -PHOTO_HEIGHT);
+    
+    UIImage *image=[UIImage imageNamed:@"timg.jpeg"];
+    //注意这个位置是相对于图层而言的不是屏幕
+    CGContextDrawImage(ctx, CGRectMake(0, 0, PHOTO_HEIGHT, PHOTO_HEIGHT), image.CGImage);
+    
+    //    CGContextFillRect(ctx, CGRectMake(0, 0, 100, 100));
+    //    CGContextDrawPath(ctx, kCGPathFillStroke);
+    
+    CGContextRestoreGState(ctx);
 }
 
 /*
